acd692beed35df28e1902554df402f5aa2e5aa2b
albertopertuz <albertopertuzamaya@gmail.com>
2022-08-18 20:15:34 -0500

    V3-Completamos probando

--- src/probando.java
+++ src/probando.java
@@ -1,4 +1,15 @@
 
 public class probando {
+ 
+    private String nombre;
+    private int precio;
+    private String categoria;
+
+    public probando(String nombre, int precio, String categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+    
     
 }
